dcec917a533b121a0e4f27681362d34c4e46a661
SergeyP <igrey@inbox.ru>
2018-02-20 09:32:03 +0300

    adventofcode11 first attempt

--- console/Program.fs
+++ console/Program.fs
@@ -1,10 +1,10 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-open adventofcode9
+open adventofcode11
 
 [<EntryPoint>]
 let main argv = 
-    let result = solve2 test
+    let result = solve test
     printf "%i" result 
     0 // return an integer exit code
