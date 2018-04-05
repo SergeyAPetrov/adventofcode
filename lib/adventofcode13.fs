a33bfed0fa0b70d5f88b327ac7dd6dbb233158e7
SergeyP <igrey@inbox.ru>
2018-04-05 16:15:06 +0300

    Solve adventofcode13 part 1

---
+++ lib/adventofcode13.fs
@@ -0,0 +1,78 @@
+module adventofcode13
+
+open System
+
+type Layer = 
+    {
+        Depth : int
+        Range : int
+        Severity : int
+    }
+    
+let parseItem (item:string) = 
+    let parts = item.Split([|": "|], StringSplitOptions.RemoveEmptyEntries)
+                    |> Array.map System.Int32.Parse
+    {Depth = parts.[0]; Range=parts.[1]; Severity = parts.[0] * parts.[1]}
+    
+let parseInput (input:string) = 
+    input.Split [|'\n'|]
+    |> Seq.map parseItem 
+    
+let isCaught layer  =
+    layer.Range = 1 || layer.Depth % (2*(layer.Range-1)) = 0 
+    
+let solve input =
+    let layers = parseInput input
+    layers 
+    |> Seq.where isCaught
+    |> Seq.map (fun l -> l.Severity)
+    |> Seq.sum
+
+let test = @"0: 3
+             1: 2
+             4: 4
+             6: 4"
+ 
+let puzzle = @"0: 3
+               1: 2
+               2: 4
+               4: 6
+               6: 4
+               8: 6
+               10: 5
+               12: 8
+               14: 8
+               16: 6
+               18: 8
+               20: 6
+               22: 10
+               24: 8
+               26: 12
+               28: 12
+               30: 8
+               32: 12
+               34: 8
+               36: 14
+               38: 12
+               40: 18
+               42: 12
+               44: 12
+               46: 9
+               48: 14
+               50: 18
+               52: 10
+               54: 14
+               56: 12
+               58: 12
+               60: 14
+               64: 14
+               68: 12
+               70: 17
+               72: 14
+               74: 12
+               76: 14
+               78: 14
+               82: 14
+               84: 14
+               94: 14
+               96: 14"
