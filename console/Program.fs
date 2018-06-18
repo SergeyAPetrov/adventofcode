90eb93cc3af302c47c77ce9b7fa21a5156a115a1
SergeyP <igrey@inbox.ru>
2018-06-19 00:00:30 +0300

    aoc16

--- console/Program.fs
+++ console/Program.fs
@@ -1,11 +1,11 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-open adventofcode14
+open adventofcode16
 open QuickGraph
 
 [<EntryPoint>]
 let main argv = 
-    let answer = solve2 "nbysizxe"
-    printf "%i" answer
+    let answer = solve2 input
+    printf "%s" (System.String.Concat(Array.ofList(answer)))
     0 // return an integer exit code
